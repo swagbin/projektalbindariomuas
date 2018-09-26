d5b8394fd45d52a23d86f649d2668b2c7f4a55fa
www2www1 <43575294+www2www1@users.noreply.github.com>
2018-09-26 11:54:59 +0200

    Update jason.js

--- Projekt2018/jason.js
+++ Projekt2018/jason.js
@@ -163,59 +163,6 @@ function validering(){
       }
     }
 
- 
 
-
-
-            ////////gamla bild spelet ////////////
-var i = 0, imgsrc = new Array(), preload = new Array();
-imgsrc[0]="photo1.jpg";
-imgsrc[1]="photo2.jpg";
-imgsrc[2]="photo3.jpg";
-imgsrc[3]="photo4.jpg";
-imgsrc[4]="photo5.jpg";
-imgsrc[5]="photo6.jpg";
-for (var j=0;j<imgsrc.length;j++)
-{
-preload[j] = new Image;
-preload[j].src = imgsrc[j];
-}
-function mode(param)
-{
-smode=param;
-}
-function startSlideshow()
-{
-if(smode=="play")
-{
-document.getElementById("play").disabled="disabled";
-document.getElementById("pause").disabled="";
-document.getElementById("stop").disabled="";
-document.getElementById("slideshow").src=imgsrc[i];
-i++;
-setTimeout("startSlideshow()",1000);
-}
-else if(smode=="pause")
-{
-document.getElementById("pause").disabled="disabled";
-document.getElementById("play").disabled="";
-document.getElementById("play").value="Resume";
-}
-else if(smode=="stop")
-{
-document.getElementById("play").disabled="";
-document.getElementById("play").value="Play";
-document.getElementById("pause").disabled="disabled";
-document.getElementById("stop").disabled="disabled";
-document.getElementById("slideshow").src=imgsrc[0];
-i=0;
-}
-if(i==imgsrc.length)
-{
-i=0;
-}
-}
-
-      
 
   
