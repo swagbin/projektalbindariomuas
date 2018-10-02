3656bde06e61d820e0d1309fe442743d1be753a5
www2www1 <43575294+www2www1@users.noreply.github.com>
2018-10-02 13:53:02 +0200

    Update jason.js

--- jason.js
+++ jason.js
@@ -38,6 +38,31 @@ $(document).ready(function () {
 
 
     });
+      });
+    $.getJSON('https://github-trending-api.now.sh/repositories?language=javascript&sort=stars&order=desc&since=weekly',
+    function (data){
+        var javascript_trends = "";
+        for(var i=0; i<5; i++){
+            javascript_trends += '<tr>';
+            javascript_trends += '<td> <a href='+data[i].url +'>'+ data[i].name+'</a></td>';
+            javascript_trends += '<td> <a href='+'https://'+'github.com/'+data[i].author+'>'+data[i].author+'</a></td>';
+            javascript_trends += '<td>' + data[i].currentPeriodStars+'</td>';
+        }
+        $('#tabellen').append(javascript_trends);
+    }
+);
+$.getJSON('https://github-trending-api.now.sh/repositories?language=css&sort=stars&order=desc&since=weekly',
+    function(output) {
+        var css_trends = "";
+        for(var i=0; i<5; i++){
+            css_trends += `<tr><td><a href="${output[i].url}">${output[i].name}</a></td>`;
+            css_trends += `<td><a href="https://github.com/${output[i].author}" target="_blank"> ${output[i].author}</a></td>`;
+            css_trends += `<td>${output[i].currentPeriodStars}</td></tr>`;
+            
+        } 
+        $('#table').append(css_trends);
+    }
+);
 
 // The function that saves userinput data as a LocalStorage, saved locally until the user presses sumbit. 
 
